655874e40ee923db46582434cfd3625bdaebab39
kongnang <brucejiu123@gmail.com>
2021-12-10 17:23:12 +0800

    version-5

--- idea_git/src/main/java/com/example/demo/GitDemo.java
+++ idea_git/src/main/java/com/example/demo/GitDemo.java
@@ -6,6 +6,6 @@ package com.example.demo;
  */
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("hello branch");
+        System.out.println("hello master");
     }
 }
