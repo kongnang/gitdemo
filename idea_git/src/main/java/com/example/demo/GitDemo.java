cadf88f90f5813ce9634b7d9d271e6257b222d2d
kongnang <brucejiu123@gmail.com>
2021-12-10 17:00:58 +0800

    version-1

---
+++ idea_git/src/main/java/com/example/demo/GitDemo.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * @author qiu
+ * @create 2021-12-10 16:54
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
