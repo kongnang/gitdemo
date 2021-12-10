652307b56fb1623d491636bc02df7cb873a3cea8
kongnang <brucejiu123@gmail.com>
2021-12-10 17:03:30 +0800

    version-2

--- idea_git/src/main/java/com/example/demo/GitDemo.java
+++ idea_git/src/main/java/com/example/demo/GitDemo.java
@@ -6,6 +6,7 @@ package com.example.demo;
  */
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
